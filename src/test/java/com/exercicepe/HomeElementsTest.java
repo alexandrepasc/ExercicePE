83a617b516311563860417824c07f7132639c383
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-18 15:47:29 +0100

    Add test to Reserved Area button.

---
+++ src/test/java/com/exercicepe/HomeElementsTest.java
@@ -0,0 +1,48 @@
+package com.exercicepe;
+
+import com.exercicepe.common.Elements;
+import com.exercicepe.common.ErrorText;
+import com.exercicepe.common.PreTest;
+import com.exercicepe.common.Translations;
+import com.exercicepe.common.Utils;
+import com.exercicepe.elements.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class HomeElementsTest extends PreTest {
+
+  @Parameters({ "browser", "gui" })
+  @BeforeMethod(alwaysRun = true)
+  public void beforeMethod(@Optional("firefox") String browser, @Optional("no") String gui)
+    throws Exception {
+
+    PreTest.beforeTest(browser, gui);
+
+    driver.get(Utils.getHomeUrl());
+
+    Assert.assertEquals(driver.getCurrentUrl(), Utils.getHomeUrl(), ErrorText.PAGE.getText());
+  }
+
+  @Test(enabled = true, invocationCount = 1)
+  public void reservedAreaTest()
+    throws Exception {
+
+    Elements element = new Elements(driver);
+
+    Assert.assertTrue(element.checkElement(HomePage.butReservedArea), ErrorText.ELEMENT.getText());
+
+    Assert.assertTrue(element.getText(HomePage.butReservedArea).contains(Translations.butReservedArea(element)),
+        ErrorText.VALUE.getText());
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void afterMethod()
+    throws Exception {
+
+    PreTest.afterTest();
+  }
+}
