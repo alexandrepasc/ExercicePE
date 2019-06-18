f59a761e4f953d9cf2fe0ad9caf7e8e72bb485ac
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-18 23:34:58 +0100

    New urls.

--- src/main/java/com/exercicepe/common/Utils.java
+++ src/main/java/com/exercicepe/common/Utils.java
@@ -11,4 +11,14 @@ public class Utils {
 
     return "https://poupaenergia.pt/entrar";
   }
+
+  public static String getGovLoginUrl() {
+
+    return "https://autenticacao.gov.pt/fa/CitizenConsent.aspx?RequestId";
+  }
+
+  public static String getAtLoginUrl() {
+
+    return "https://www.acesso.gov.pt/jsp/loginRedirectForm.jsp";
+  }
 }
