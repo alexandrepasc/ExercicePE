c98d26ddae52760d024664aed0dee8379c811f54
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-19 00:32:59 +0100

    Add test cases. New elements.

--- src/main/java/com/exercicepe/elements/LoginPage.java
+++ src/main/java/com/exercicepe/elements/LoginPage.java
@@ -9,8 +9,14 @@ public class LoginPage {
   public static By inputMail = By
       .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(3) > label > input");
 
+  public static By labelMailFeedback = By
+      .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(3) > label > span");
+
   public static By inputPassword = By
       .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(4) > label > input");
+
+  public static By labelPasswordFeedback = By
+      .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(4) > label > span");
 
   public static By butLogin = By
       .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(5) > div > button");
@@ -19,6 +25,9 @@ public class LoginPage {
 
   public static By butGovAuth = By
       .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(9) > form > button");
+
+  public static By imgGovHeader = By.cssSelector(
+      "#aspnetForm > div.container.container-page.container-page-height > div.fd > div.row.logo > div > a > img");
 
   public static By butATAuth = By
       .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div.sc-iwsKbI.ctZuQQ > form > button");
