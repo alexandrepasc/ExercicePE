181d7ea3113e21d072521a0334371169ba0e513a
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-18 23:05:38 +0100

    Add new elements.

--- src/main/java/com/exercicepe/elements/LoginPage.java
+++ src/main/java/com/exercicepe/elements/LoginPage.java
@@ -6,19 +6,23 @@ public class LoginPage {
 
   public static By labelTitle = By.cssSelector("#adene-login > div > h2");
 
-  public static By inputMail = By.cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(3) > label > input");
+  public static By inputMail = By
+      .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(3) > label > input");
 
   public static By inputPassword = By
       .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(4) > label > input");
 
-  public static By butLogin = By.cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(5) > div > button");
+  public static By butLogin = By
+      .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(5) > div > button");
 
   public static By butRecoverPwd = By.cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > a");
 
-  public static By butGovAuth = By.cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(9) > form > button");
+  public static By butGovAuth = By
+      .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div:nth-child(9) > form > button");
 
   public static By butATAuth = By
       .cssSelector("#auth-card > div > div.sc-kAzzGY.bNvMpU > div.sc-iwsKbI.ctZuQQ > form > button");
 
-  public static By butRegister = By.cssSelector("#auth-card > div > div.sc-gZMcBi.gwjsYB > div.sc-iwsKbI.kxdJwJ > button");
+  public static By butRegister = By
+      .cssSelector("#auth-card > div > div.sc-gZMcBi.gwjsYB > div.sc-iwsKbI.kxdJwJ > button");
 }
