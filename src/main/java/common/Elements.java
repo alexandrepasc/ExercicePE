9cced490f05fd070ea621910a219c9ac483cd7fa
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-18 08:33:20 +0100

    Add Elements and ErrorText classes.

---
+++ src/main/java/common/Elements.java
@@ -0,0 +1,75 @@
+package common;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Elements {
+
+  private WebDriver driver;
+
+  public Elements(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public WebElement getElement(By by) {
+    try {
+
+      return driver.findElement(by);
+
+    } catch (StaleElementReferenceException e) {
+
+      e.printStackTrace();
+      return null;
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  public WebElement[] getElements(By by) {
+    try {
+
+      return driver.findElements(by).toArray(new WebElement[0]);
+
+    } catch (StaleElementReferenceException e) {
+
+      e.printStackTrace();
+      return null;
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  public String getElementText(By by) {
+    try {
+
+      return getElement(by).getText();
+
+    } catch (StaleElementReferenceException e) {
+
+      e.printStackTrace();
+      return null;
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  public String getElementText(By by, int numb) {
+    try {
+
+      return getElements(by)[numb].getText();
+
+    } catch (StaleElementReferenceException e) {
+
+      e.printStackTrace();
+      return null;
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
+}
